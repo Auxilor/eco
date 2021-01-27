94bf383d234a317ff86b1c653e85e5db5cc95fa5
Auxilor <will@willfp.com>
2021-01-27 09:56:23 +0000

    Added setType to DropManager

--- eco-util/src/main/java/com/willfp/eco/util/drops/internal/DropManager.java
+++ eco-util/src/main/java/com/willfp/eco/util/drops/internal/DropManager.java
@@ -2,14 +2,24 @@ package com.willfp.eco.util.drops.internal;
 
 import lombok.Getter;
 import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.NotNull;
 
 @UtilityClass
 public final class DropManager {
     /**
      * The currently used type, or implementation, of {@link AbstractDropQueue}.
      * <p>
-     * Standard by default, used if drops.collate key is not present in config.
+     * Default is {@link DropQueueType#COLLATED}, however this can be changed.
      */
     @Getter
     private DropQueueType type = DropQueueType.COLLATED;
+
+    /**
+     * Sets the type of Drop Queue to be used.
+     *
+     * @param type The type.
+     */
+    public static void setType(@NotNull final DropQueueType type) {
+        DropManager.type = type;
+    }
 }
