9be516e5110fcb9435ef51fb2dae834d63312dc7
Auxilor <william.favierparsons1@gmail.com>
2021-04-26 21:24:28 +0100

    Added villager-display-fix to force remove all hideflags

--- eco-core/core-plugin/src/main/java/com/willfp/eco/spigot/display/PacketOpenWindowMerchant.java
+++ eco-core/core-plugin/src/main/java/com/willfp/eco/spigot/display/PacketOpenWindowMerchant.java
@@ -8,8 +8,12 @@ import com.willfp.eco.core.AbstractPacketAdapter;
 import com.willfp.eco.core.EcoPlugin;
 import com.willfp.eco.proxy.proxies.VillagerTradeProxy;
 import com.willfp.eco.spigot.InternalProxyUtils;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -29,8 +33,19 @@ public class PacketOpenWindowMerchant extends AbstractPacketAdapter {
     public void onSend(@NotNull final PacketContainer packet,
                        @NotNull final Player player,
                        @NotNull final PacketEvent event) {
-
         List<MerchantRecipe> recipes = new ArrayList<>();
+
+        if (((EcoPlugin) this.getPlugin()).getConfigYml().getBool("villager-display-fix")) {
+            for (MerchantRecipe recipe : packet.getMerchantRecipeLists().read(0)) {
+                ItemStack result = recipe.getResult();
+                ItemMeta meta = result.getItemMeta();
+                if (meta != null) {
+                    meta.removeItemFlags(ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_POTION_EFFECTS);
+                    meta.getPersistentDataContainer().remove(NamespacedKey.fromString("ecoenchants:ecoenchantlore-skip"));
+                    result.setItemMeta(meta);
+                }
+            }
+        }
 
         for (MerchantRecipe recipe : packet.getMerchantRecipeLists().read(0)) {
             MerchantRecipe newRecipe = InternalProxyUtils.getProxy(VillagerTradeProxy.class).displayTrade(recipe);
