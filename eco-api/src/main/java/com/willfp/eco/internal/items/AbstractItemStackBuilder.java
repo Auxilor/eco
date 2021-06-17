74ca2e82efc98934ad4e7de6717de841c705f6df
Auxilor <william.favierparsons1@gmail.com>
2021-06-17 11:14:47 +0100

    Added suppliers to itemstackbuilder

--- eco-api/src/main/java/com/willfp/eco/internal/items/AbstractItemStackBuilder.java
+++ eco-api/src/main/java/com/willfp/eco/internal/items/AbstractItemStackBuilder.java
@@ -16,6 +16,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 @SuppressWarnings("unchecked")
 public abstract class AbstractItemStackBuilder<T extends ItemMeta> {
@@ -64,6 +65,16 @@ public abstract class AbstractItemStackBuilder<T extends ItemMeta> {
     }
 
     /**
+     * Set the ItemStack amount.
+     *
+     * @param amount The amount.
+     * @return The builder.
+     */
+    public AbstractItemStackBuilder<T> setAmount(@NotNull final Supplier<Integer> amount) {
+        return setAmount(amount.get());
+    }
+
+    /**
      * Add an enchantment to the item.
      *
      * @param enchantment The enchantment.
@@ -77,6 +88,18 @@ public abstract class AbstractItemStackBuilder<T extends ItemMeta> {
     }
 
     /**
+     * Add an enchantment to the item.
+     *
+     * @param enchantment The enchantment.
+     * @param level       The level.
+     * @return The builder.
+     */
+    public AbstractItemStackBuilder<T> addEnchantment(@NotNull final Supplier<Enchantment> enchantment,
+                                                      @NotNull final Supplier<Integer> level) {
+        return addEnchantment(enchantment.get(), level.get());
+    }
+
+    /**
      * Set the item display name.
      *
      * @param name The name.
@@ -85,6 +108,18 @@ public abstract class AbstractItemStackBuilder<T extends ItemMeta> {
     public AbstractItemStackBuilder<T> setDisplayName(@NotNull final String name) {
         meta.setDisplayName(StringUtils.translate(name));
         return this;
+    }
+
+    /**
+     * Set the item display name.
+     *
+     * @param name The name.
+     * @return The builder.
+     */
+    public AbstractItemStackBuilder<T> setDisplayName(@NotNull final Supplier<String> name) {
+        String result = name.get();
+
+        return result == null ? this : setDisplayName(name.get());
     }
 
     /**
@@ -100,6 +135,18 @@ public abstract class AbstractItemStackBuilder<T extends ItemMeta> {
         meta.setLore(lore);
 
         return this;
+    }
+
+    /**
+     * Add lore line.
+     *
+     * @param line The line.
+     * @return The builder.
+     */
+    public AbstractItemStackBuilder<T> addLoreLine(@NotNull final Supplier<String> line) {
+        String result = line.get();
+
+        return result == null ? this : addLoreLine(line.get());
     }
 
     /**
@@ -120,6 +167,18 @@ public abstract class AbstractItemStackBuilder<T extends ItemMeta> {
     }
 
     /**
+     * Add lore lines.
+     *
+     * @param lines The lines.
+     * @return The builder.
+     */
+    public AbstractItemStackBuilder<T> addLoreLines(@NotNull final Supplier<List<String>> lines) {
+        List<String> result = lines.get();
+
+        return result == null ? this : addLoreLines(lines.get());
+    }
+
+    /**
      * Add ItemFlags.
      *
      * @param itemFlags The flags.
@@ -129,6 +188,18 @@ public abstract class AbstractItemStackBuilder<T extends ItemMeta> {
         meta.addItemFlags(itemFlags);
 
         return this;
+    }
+
+    /**
+     * Add ItemFlags.
+     *
+     * @param itemFlags The flags.
+     * @return The builder.
+     */
+    public AbstractItemStackBuilder<T> addItemFlag(@NotNull final Supplier<ItemFlag[]> itemFlags) {
+        ItemFlag[] result = itemFlags.get();
+
+        return result == null ? this : addItemFlag(result);
     }
 
     /**
@@ -150,6 +221,22 @@ public abstract class AbstractItemStackBuilder<T extends ItemMeta> {
     }
 
     /**
+     * Write meta key.
+     *
+     * @param key   The key.
+     * @param type  The type.
+     * @param value The value.
+     * @param <A>   The type.
+     * @param <B>   The type.
+     * @return The builder.
+     */
+    public <A, B> AbstractItemStackBuilder<T> writeMetaKey(@NotNull final Supplier<NamespacedKey> key,
+                                                           @NotNull final Supplier<PersistentDataType<A, B>> type,
+                                                           @NotNull final Supplier<B> value) {
+        return writeMetaKey(key.get(), type.get(), value.get());
+    }
+
+    /**
      * Set unbreakable.
      *
      * @param unbreakable If the item should be unbreakable.
@@ -162,6 +249,18 @@ public abstract class AbstractItemStackBuilder<T extends ItemMeta> {
     }
 
     /**
+     * Set unbreakable.
+     *
+     * @param unbreakable If the item should be unbreakable.
+     * @return The builder.
+     */
+    public AbstractItemStackBuilder<T> setUnbreakable(@NotNull final Supplier<Boolean> unbreakable) {
+        Boolean result = unbreakable.get();
+
+        return result == null ? this : setUnbreakable(unbreakable);
+    }
+
+    /**
      * Set custom model data.
      *
      * @param data The data.
@@ -171,6 +270,18 @@ public abstract class AbstractItemStackBuilder<T extends ItemMeta> {
         meta.setCustomModelData(data);
 
         return this;
+    }
+
+    /**
+     * Set custom model data.
+     *
+     * @param data The data.
+     * @return The builder.
+     */
+    public AbstractItemStackBuilder<T> setCustomModelData(@NotNull final Supplier<Integer> data) {
+        Integer result = data.get();
+
+        return result == null ? this : setCustomModelData(result);
     }
 
     /**
