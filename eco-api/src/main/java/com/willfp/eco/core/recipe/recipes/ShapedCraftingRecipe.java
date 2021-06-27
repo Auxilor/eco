ec8a65b4f863a1648a0bebf365c92188e0f7ab90
Auxilor <william.favierparsons1@gmail.com>
2021-06-27 15:59:55 +0100

    Removed redundant suppression

--- eco-api/src/main/java/com/willfp/eco/core/recipe/recipes/ShapedCraftingRecipe.java
+++ eco-api/src/main/java/com/willfp/eco/core/recipe/recipes/ShapedCraftingRecipe.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@SuppressWarnings("deprecation")
 public final class ShapedCraftingRecipe extends PluginDependent implements CraftingRecipe {
     /**
      * Recipe parts.
