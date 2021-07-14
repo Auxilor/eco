b60fc49fd546b3671f80b2726ef8f45a10e2997f
Auxilor <william.favierparsons1@gmail.com>
2021-07-14 16:13:00 +0200

    Tweaked PlayerJumpEvent

--- eco-api/src/main/java/com/willfp/eco/core/events/PlayerJumpEvent.java
+++ eco-api/src/main/java/com/willfp/eco/core/events/PlayerJumpEvent.java
@@ -1,8 +1,10 @@
 package com.willfp.eco.core.events;
 
+import org.bukkit.Location;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class PlayerJumpEvent extends PlayerMoveEvent {
     /**
@@ -11,12 +13,19 @@ public class PlayerJumpEvent extends PlayerMoveEvent {
     private static final HandlerList HANDLERS = new HandlerList();
 
     /**
+     * The handled event.
+     */
+    private final PlayerMoveEvent handle;
+
+    /**
      * Create a new PlayerJumpEvent.
      *
      * @param event The PlayerMoveEvent.
      */
     public PlayerJumpEvent(@NotNull final PlayerMoveEvent event) {
         super(event.getPlayer(), event.getFrom(), event.getTo());
+
+        this.handle = event;
     }
 
     /**
@@ -36,5 +45,37 @@ public class PlayerJumpEvent extends PlayerMoveEvent {
      */
     public static @NotNull HandlerList getHandlerList() {
         return HANDLERS;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return handle.isCancelled();
+    }
+
+    @Override
+    public void setCancelled(final boolean cancel) {
+        handle.setCancelled(cancel);
+    }
+
+    @NotNull
+    @Override
+    public Location getFrom() {
+        return handle.getFrom();
+    }
+
+    @Override
+    public void setFrom(@NotNull final Location from) {
+        handle.setFrom(from);
+    }
+
+    @Nullable
+    @Override
+    public Location getTo() {
+        return handle.getTo();
+    }
+
+    @Override
+    public void setTo(@NotNull final Location to) {
+        handle.setTo(to);
     }
 }
