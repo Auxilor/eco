8650e9bb2ada320ecdcc674ed5928c260134c322
Auxilor <william.favierparsons1@gmail.com>
2021-07-16 14:34:24 +0200

    Added javadoc to Eco#getHandler

--- eco-api/src/main/java/com/willfp/eco/core/Eco.java
+++ eco-api/src/main/java/com/willfp/eco/core/Eco.java
@@ -10,6 +10,13 @@ import org.jetbrains.annotations.NotNull;
 public class Eco {
     /**
      * Instance of eco handler.
+     * <p>
+     * The handler is, in essence, a way to interface between the eco-api
+     * frontend module, and the eco-backend implementations.
+     * <p>
+     * There shouldn't really be any reason to ever use the handler
+     * in your own plugins, but if you want to then you can - it's
+     * just a part of the API like any other.
      */
     @Getter
     private Handler handler;
