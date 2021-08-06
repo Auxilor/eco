8828f8e45d9a0fa152b894cb25a0e20e2a754f49
Auxilor <william.favierparsons1@gmail.com>
2021-08-06 20:05:57 +0100

    Moved EcoConfigHandler to kotlin

---
+++ eco-core/core-backend/src/main/kotlin/com/willfp/eco/internal/config/updating/EcoConfigHandler.kt
@@ -0,0 +1,66 @@
+package com.willfp.eco.internal.config.updating
+
+import com.willfp.eco.core.EcoPlugin
+import com.willfp.eco.core.PluginDependent
+import com.willfp.eco.core.config.interfaces.LoadableConfig
+import com.willfp.eco.core.config.updating.ConfigHandler
+import com.willfp.eco.core.config.updating.ConfigUpdater
+import com.willfp.eco.internal.config.json.EcoLoadableJSONConfig
+import com.willfp.eco.internal.config.updating.exceptions.InvalidUpdateMethodException
+import com.willfp.eco.internal.config.yaml.EcoLoadableYamlConfig
+import com.willfp.eco.internal.config.yaml.EcoUpdatableYamlConfig
+import org.reflections.Reflections
+import org.reflections.scanners.MethodAnnotationsScanner
+import java.lang.reflect.Modifier
+
+class EcoConfigHandler(
+    plugin: EcoPlugin
+) : PluginDependent<EcoPlugin>(plugin), ConfigHandler {
+    private val reflections: Reflections = Reflections(
+        this.plugin::class.java.classLoader,
+        MethodAnnotationsScanner()
+    )
+
+    private val configs: MutableList<LoadableConfig> = ArrayList()
+
+    override fun callUpdate() {
+        for (method in reflections.getMethodsAnnotatedWith(ConfigUpdater::class.java)) {
+            if (!Modifier.isStatic(method.modifiers)) {
+                throw InvalidUpdateMethodException("Update method must be static.")
+            }
+
+            try {
+                if (method.parameterCount == 0) {
+                    method.invoke(null)
+                } else if (method.parameterCount == 1) {
+                    method.invoke(null, this.plugin)
+                } else {
+                    throw InvalidUpdateMethodException("Update method must have 0 parameters or a plugin parameter.")
+                }
+            } catch (e: ReflectiveOperationException) {
+                e.printStackTrace()
+                throw InvalidUpdateMethodException("Update method generated an exception")
+            }
+        }
+    }
+
+    override fun saveAllConfigs() {
+        for (config in configs) {
+            config.save()
+        }
+    }
+
+    override fun addConfig(config: LoadableConfig) {
+        configs.add(config)
+    }
+
+    override fun updateConfigs() {
+        for (config in configs) {
+            when (config) {
+                is EcoUpdatableYamlConfig -> config.update()
+                is EcoLoadableYamlConfig -> config.reloadFromFile()
+                is EcoLoadableJSONConfig -> config.reloadFromFile()
+            }
+        }
+    }
+}
---
+++ eco-core/core-backend/src/main/kotlin/com/willfp/eco/internal/fast/EcoFastItemStack.kt
@@ -0,0 +1,13 @@
+package com.willfp.eco.internal.fast;
+
+import com.willfp.eco.core.fast.FastItemStack
+import org.bukkit.inventory.ItemStack
+
+abstract class EcoFastItemStack<T>(
+    val handle: T,
+    val bukkit: ItemStack
+) : FastItemStack {
+    override fun unwrap(): ItemStack {
+        return bukkit
+    }
+}
