f953064e909e0be177e40e14587092be64a532ca
Conclure <thealql77@gmail.com>
2021-08-08 18:23:16 +0200

    Derive ConfigurationSerializable, PersistentDataHolder and add new methods including default implementations and delegate serialization to FastItemStackSerialization

--- eco-api/src/main/java/com/willfp/eco/core/fast/FastItemStack.java
+++ eco-api/src/main/java/com/willfp/eco/core/fast/FastItemStack.java
@@ -2,19 +2,27 @@ package com.willfp.eco.core.fast;
 
 import com.willfp.eco.core.Eco;
 import org.bukkit.Material;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.configuration.serialization.DelegateDeserialization;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataHolder;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * FastItemStack contains methods to modify and read items faster than in default bukkit.
  */
-public interface FastItemStack {
+@DelegateDeserialization(FastItemStackSerialization.class)
+public interface FastItemStack extends PersistentDataHolder, ConfigurationSerializable {
+
+    boolean hasEnchantmentsOnItem();
+
     /**
      * Get all enchantments on an item.
      *
@@ -33,12 +41,18 @@ public interface FastItemStack {
     int getLevelOnItem(@NotNull Enchantment enchantment,
                        boolean checkStored);
 
+    boolean hasLore();
+
     /**
      * Set the item lore.
      *
      * @param lore The lore.
      */
     void setLore(@Nullable List<String> lore);
+
+    default void setLore(String... lore) {
+        this.setLore(Arrays.asList(lore));
+    }
 
     /**
      * Get the item lore.
@@ -47,11 +61,67 @@ public interface FastItemStack {
      */
     List<String> getLore();
 
+    boolean hasItemFlags();
+
+    void setItemFlags(Set<ItemFlag> itemFlags);
+
+    default void setItemFlags(ItemFlag... itemFlags) {
+        Set<ItemFlag> flags = EnumSet.noneOf(ItemFlag.class);
+        flags.addAll(Arrays.asList(itemFlags));
+        this.setItemFlags(flags);
+    }
+
+    Set<ItemFlag> getItemFlags();
+
+    boolean hasDisplayName();
+
+    void setDisplayName(String displayName);
+
+    String getDisplayName();
+
+    void setUnbreakable(boolean unbreakable);
+
+    boolean isUnbreakable();
+
+    void setType(Material material);
+
+    Material getType();
+
+    boolean isStackable();
+
+    default boolean isUnstackable() {
+        return !this.isStackable();
+    }
+
+    void setAmount(int amount);
+
+    int getAmount();
+
+    default void shrink(int amount) {
+        this.setAmount(this.getAmount()-amount);
+    }
+
+    default void shrink() {
+        this.shrink(1);
+    }
+
+    int getDurability();
+
+    void setDurability(int durability);
+
+    boolean isSimilar(FastItemStack itemStack);
+
+    default boolean isSimilar(ItemStack itemStack) {
+        return this.isSimilar(FastItemStack.wrap(itemStack));
+    }
+
     /**
      * Get the Bukkit ItemStack again.
      *
      * @return The ItemStack.
      */
+    @Contract("-> new")
+    @NotNull
     ItemStack unwrap();
 
     /**
@@ -60,7 +130,18 @@ public interface FastItemStack {
      * @param itemStack The ItemStack.
      * @return The FastItemStack.
      */
+    @Contract("_ -> new")
+    @NotNull
     static FastItemStack wrap(final ItemStack itemStack) {
         return Eco.getHandler().createFastItemStack(Objects.requireNonNullElseGet(itemStack, () -> new ItemStack(Material.AIR)));
     }
+
+    @NotNull
+    @Override
+    PersistentDataContainer getPersistentDataContainer();
+
+    @Contract("-> new")
+    @NotNull
+    @Override
+    Map<String, Object> serialize();
 }
