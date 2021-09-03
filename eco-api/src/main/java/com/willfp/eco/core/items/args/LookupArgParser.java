94b73ef35c9ed1d1f02d973ff29ad9a5a99dd331
Auxilor <william.favierparsons1@gmail.com>
2021-09-03 12:49:59 +0100

    Improved Arg Parser javadoc

--- eco-api/src/main/java/com/willfp/eco/core/items/args/LookupArgParser.java
+++ eco-api/src/main/java/com/willfp/eco/core/items/args/LookupArgParser.java
@@ -1,5 +1,6 @@
 package com.willfp.eco.core.items.args;
 
+import com.willfp.eco.core.items.TestableItem;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
@@ -9,6 +10,9 @@ import java.util.function.Predicate;
 public interface LookupArgParser {
     /**
      * Parse the arguments.
+     * <p>
+     * An argument parser should generate the predicate as well
+     * as modify the ItemMeta for {@link TestableItem#getItem()}
      *
      * @param args The arguments.
      * @param meta The ItemMeta to modify.
