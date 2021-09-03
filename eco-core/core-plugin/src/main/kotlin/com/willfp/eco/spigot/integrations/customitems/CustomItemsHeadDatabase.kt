1ebf7fb875654b917a6cf355a1a5b81d29f0b06b
Auxilor <william.favierparsons1@gmail.com>
2021-09-03 11:45:34 +0100

    Added Head Database integration

---
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/spigot/integrations/customitems/CustomItemsHeadDatabase.kt
@@ -0,0 +1,34 @@
+package com.willfp.eco.spigot.integrations.customitems
+
+import com.willfp.eco.core.integrations.customitems.CustomItemsWrapper
+import com.willfp.eco.core.items.CustomItem
+import com.willfp.eco.util.NamespacedKeyUtils
+import me.arcaniax.hdb.api.HeadDatabaseAPI
+import me.arcaniax.hdb.enums.CategoryEnum
+import java.util.function.Predicate
+
+class CustomItemsHeadDatabase : CustomItemsWrapper {
+    private val api = HeadDatabaseAPI()
+
+    override fun registerAllItems() {
+        for (categoryEnum in CategoryEnum.values()) {
+            for (head in api.getHeads(categoryEnum)) {
+                val stack = head.head
+                val id = head.id
+                val key = NamespacedKeyUtils.create("headdb", id.lowercase());
+                CustomItem(
+                    key,
+                    Predicate { test ->
+                        val headId = api.getItemID(test) ?: return@Predicate false
+                        headId.equals(id, ignoreCase = true)
+                    },
+                    stack
+                ).register()
+            }
+        }
+    }
+
+    override fun getPluginName(): String {
+        return "HeadDatabase"
+    }
+}
