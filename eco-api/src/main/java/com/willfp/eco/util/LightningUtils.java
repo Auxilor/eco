a65f16cfac87cbac62c6a07c7e3cdb4a935bd32f
Auxilor <william.favierparsons1@gmail.com>
2021-09-14 08:34:51 +0100

    Improved PR

--- eco-api/src/main/java/com/willfp/eco/util/LightningUtils.java
+++ eco-api/src/main/java/com/willfp/eco/util/LightningUtils.java
@@ -3,7 +3,6 @@ package com.willfp.eco.util;
 import lombok.experimental.UtilityClass;
 import org.bukkit.Location;
 import org.bukkit.Sound;
-import org.bukkit.SoundCategory;
 import org.bukkit.entity.LivingEntity;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,21 +20,26 @@ public class LightningUtils {
      * @param silent If the lightning sound should be played locally
      */
     public void strike(@NotNull final LivingEntity victim,
-                       final double damage, boolean silent) {
+                       final double damage,
+                       final boolean silent) {
         Location loc = victim.getLocation();
 
         if (silent) {
             victim.getWorld().spigot().strikeLightningEffect(loc, true);
             victim.getWorld().playSound(loc, Sound.ENTITY_LIGHTNING_BOLT_IMPACT, 1, 1);
-        }
-        else {
+        } else {
             victim.getWorld().strikeLightningEffect(loc);
         }
 
         victim.damage(damage);
     }
 
-
+    /**
+     * Strike lightning on a victim without fire.
+     *
+     * @param victim The entity to smite.
+     * @param damage The damage to deal.
+     */
     public void strike(@NotNull final LivingEntity victim,
                        final double damage) {
         strike(victim, damage, false);
