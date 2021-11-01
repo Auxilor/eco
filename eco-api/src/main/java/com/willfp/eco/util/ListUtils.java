d952dbc61b1bffebffab11fb1307514fde8afba0
Auxilor <william.favierparsons1@gmail.com>
2021-11-01 20:00:46 +0000

    Added ListUtils#listToFrequencyMap

--- eco-api/src/main/java/com/willfp/eco/util/ListUtils.java
+++ eco-api/src/main/java/com/willfp/eco/util/ListUtils.java
@@ -5,7 +5,9 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Utilities / API methods for lists.
@@ -33,5 +35,26 @@ public class ListUtils {
         }
 
         return list;
+    }
+
+    /**
+     * Convert a list potentially containing duplicates to a map where the value is the frequency of the key.
+     *
+     * @param list The list.
+     * @param <T>  The type parameter of the list.
+     * @return The frequency map.
+     */
+    @NotNull
+    private static <T> Map<T, Integer> listToFrequencyMap(@NotNull final List<T> list) {
+        Map<T, Integer> frequencyMap = new HashMap<>();
+        for (T object : list) {
+            if (frequencyMap.containsKey(object)) {
+                frequencyMap.put(object, frequencyMap.get(object) + 1);
+            } else {
+                frequencyMap.put(object, 1);
+            }
+        }
+
+        return frequencyMap;
     }
 }
