81196bb13254102da82d18883ca7e067a6b80d3e
Auxilor <william.favierparsons1@gmail.com>
2021-11-01 18:44:44 +0000

    Added PotionUtils

---
+++ eco-api/src/main/java/com/willfp/eco/util/PotionUtils.java
@@ -0,0 +1,47 @@
+package com.willfp.eco.util;
+
+import lombok.experimental.UtilityClass;
+import org.bukkit.potion.PotionData;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Utilities / API methods for potions.
+ */
+@UtilityClass
+public class PotionUtils {
+    /**
+     * Get the duration (in ticks) for potion data.
+     *
+     * @param data The data.
+     * @return The duration.
+     */
+    public int getDuration(@NotNull final PotionData data) {
+        if (data.isExtended()) {
+            return switch (data.getType()) {
+                case INSTANT_DAMAGE, INSTANT_HEAL: yield 0;
+                case POISON, REGEN: yield 1800;
+                case SLOW_FALLING, WEAKNESS, SLOWNESS: yield 4800;
+                case TURTLE_MASTER: yield 800;
+                default: yield 9600;
+            };
+        }
+
+        if (data.isUpgraded()) {
+            return switch (data.getType()) {
+                case INSTANT_DAMAGE, INSTANT_HEAL: yield 0;
+                case POISON, REGEN: yield 420;
+                case SLOW_FALLING, WEAKNESS, SLOWNESS: yield 440;
+                case TURTLE_MASTER: yield 400;
+                default: yield 1800;
+            };
+        }
+
+        return switch (data.getType()) {
+            case INSTANT_DAMAGE, INSTANT_HEAL: yield 0;
+            case POISON, REGEN: yield 900;
+            case SLOW_FALLING, WEAKNESS, SLOWNESS: yield 400;
+            case TURTLE_MASTER: yield 1800;
+            default: yield 3600;
+        };
+    }
+}
