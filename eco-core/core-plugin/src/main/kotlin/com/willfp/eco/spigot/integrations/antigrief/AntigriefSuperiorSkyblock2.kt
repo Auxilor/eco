abad9730519712985823dd8131f2c7c6b0fc8f4e
Auxilor <william.favierparsons1@gmail.com>
2021-11-13 11:06:03 +0000

    PR Codestyle

--- eco-core/core-plugin/src/main/kotlin/com/willfp/eco/spigot/integrations/antigrief/AntigriefSuperiorSkyblock2.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/spigot/integrations/antigrief/AntigriefSuperiorSkyblock2.kt
@@ -17,24 +17,32 @@ class AntigriefSuperiorSkyblock2 : AntigriefWrapper {
     }
 
     override fun canBreakBlock(player: Player, block: Block): Boolean {
-        if (SuperiorSkyblockAPI.getPlayer(player).hasBypassModeEnabled()) return true
+        if (SuperiorSkyblockAPI.getPlayer(player).hasBypassModeEnabled()) {
+            return true
+        }
         return SuperiorSkyblockAPI.getPlayer(player).hasPermission(IslandPrivilege.getByName("Break"))
                 || SuperiorSkyblockAPI.getPlayer(player).hasPermission(IslandPrivilege.getByName("BREAK"))
     }
 
     override fun canCreateExplosion(player: Player, location: Location): Boolean {
-        if (SuperiorSkyblockAPI.getPlayer(player).hasBypassModeEnabled()) return true
+        if (SuperiorSkyblockAPI.getPlayer(player).hasBypassModeEnabled()) {
+            return true
+        }
         return SuperiorSkyblockAPI.getIslandAt(location)?.isMember(SuperiorSkyblockAPI.getPlayer(player)) ?: true
     }
 
     override fun canPlaceBlock(player: Player, block: Block): Boolean {
-        if (SuperiorSkyblockAPI.getPlayer(player).hasBypassModeEnabled()) return true
+        if (SuperiorSkyblockAPI.getPlayer(player).hasBypassModeEnabled()) {
+            return true
+        }
         return SuperiorSkyblockAPI.getPlayer(player).hasPermission(IslandPrivilege.getByName("Place"))
                 || SuperiorSkyblockAPI.getPlayer(player).hasPermission(IslandPrivilege.getByName("PLACE"))
     }
 
     override fun canInjure(player: Player, victim: LivingEntity): Boolean {
-        if (SuperiorSkyblockAPI.getPlayer(player).hasBypassModeEnabled()) return true
+        if (SuperiorSkyblockAPI.getPlayer(player).hasBypassModeEnabled()) {
+            return true
+        }
         return when (victim) {
             is Player -> SuperiorSkyblockAPI.getPlayer(player).canHit(SuperiorSkyblockAPI.getPlayer(victim)).equals(HitActionResult.SUCCESS)
             is Animals -> {
