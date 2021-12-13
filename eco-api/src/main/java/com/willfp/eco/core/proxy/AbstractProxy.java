e595ea224731cd253e1136b5293097e961958888
Auxilor <william.favierparsons1@gmail.com>
2021-12-13 12:09:58 +0000

    Marked AbstractProxy for removal

--- eco-api/src/main/java/com/willfp/eco/core/proxy/AbstractProxy.java
+++ eco-api/src/main/java/com/willfp/eco/core/proxy/AbstractProxy.java
@@ -4,9 +4,9 @@ package com.willfp.eco.core.proxy;
  * Prior to 6.17.0, all proxies were required to implement this interface,
  * however it produced no functionality and was not even used internally.
  *
- * @deprecated Unused class, not required.
+ * @deprecated Unused class, not required. Will be removed in a subsequent release.
  */
-@Deprecated
+@Deprecated(since = "6.17.0", forRemoval = true)
 public interface AbstractProxy {
 
 }
