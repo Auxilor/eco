ed46900f2fa22290feaa7996e65ea7684813d13e
Auxilor <william.favierparsons1@gmail.com>
2022-02-02 12:20:33 +0000

    Re-added NMS Modules after build checks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,12 +12,10 @@ include(":eco-api")
 include(":eco-api:api-java")
 include(":eco-api:api-kotlin")
 include(":eco-core")
-/*
 include(":eco-core:core-nms")
 include(":eco-core:core-nms:v1_16_R3")
 include(":eco-core:core-nms:v1_17_R1")
 include(":eco-core:core-nms:v1_18_R1")
- */
 include(":eco-core:core-proxy")
 include(":eco-core:core-plugin")
 include(":eco-core:core-backend")
