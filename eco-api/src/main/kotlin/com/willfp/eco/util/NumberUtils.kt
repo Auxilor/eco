f3c69f1c1563259e8dfec175334d8862441d8073
Auxilor <william.favierparsons1@gmail.com>
2022-02-03 11:08:20 +0000

    Added NumberUtils extensions

---
+++ eco-api/src/main/kotlin/com/willfp/eco/util/NumberUtils.kt
@@ -0,0 +1,9 @@
+@file:JvmName("NumberUtilsExtensions")
+
+package com.willfp.eco.util
+
+/**
+ * @see NumberUtils.toNumeral
+ */
+fun Number.toNumeral(): String =
+    NumberUtils.toNumeral(this.toInt())
