b9d50e261d141f0cd9db19d9db5c51d1520dd5ee
Auxilor <william.favierparsons1@gmail.com>
2022-02-03 13:44:15 +0000

    Fixed missing javadoc

--- eco-api/src/main/java/com/willfp/eco/core/EcoPluginProps.java
+++ eco-api/src/main/java/com/willfp/eco/core/EcoPluginProps.java
@@ -4,6 +4,17 @@ import com.willfp.eco.core.config.interfaces.Config;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Plugin props are the arguments related to the plugin that are required on start-up.
+ * Marked as internal as the props may change / update, which will cause problems if
+ * you create props objects.
+ *
+ * @param resourceId           The ID of the plugin on polymart.
+ * @param bStatsId             The ID of the plugin on bStats.
+ * @param proxyPackage         The package where proxies can be found.
+ * @param color                The primary color of the plugin.
+ * @param supportingExtensions If the plugin should attempt to look for extensions.
+ */
 @ApiStatus.Internal
 public record EcoPluginProps(int resourceId,
                              int bStatsId,
