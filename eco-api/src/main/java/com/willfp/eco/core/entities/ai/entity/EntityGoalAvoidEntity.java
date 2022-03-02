4f009c6d5757d786139913b672adf65d3f09eeb7
Auxilor <william.favierparsons1@gmail.com>
2022-03-02 17:45:40 +0000

    EntityGoalAvoidEntity now functions using TestableEntity

--- eco-api/src/main/java/com/willfp/eco/core/entities/ai/entity/EntityGoalAvoidEntity.java
+++ eco-api/src/main/java/com/willfp/eco/core/entities/ai/entity/EntityGoalAvoidEntity.java
@@ -6,30 +6,23 @@ import com.willfp.eco.core.entities.TestableEntity;
 import com.willfp.eco.core.entities.ai.EntityGoal;
 import com.willfp.eco.core.serialization.KeyedDeserializer;
 import org.bukkit.NamespacedKey;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Mob;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Objects;
-import java.util.function.Predicate;
-
 /**
  * Avoid entities.
  *
- * @param avoidClass   The entity classes to avoid.
+ * @param entity       The entity type to avoid.
  * @param fleeDistance The distance to flee to.
  * @param slowSpeed    The slow movement speed.
  * @param fastSpeed    The fast movement speed.
- * @param filter       The filter for entities to check if they should be avoided.
  */
 public record EntityGoalAvoidEntity(
-        @NotNull Class<? extends LivingEntity> avoidClass,
+        @NotNull TestableEntity entity,
         double fleeDistance,
         double slowSpeed,
-        double fastSpeed,
-        @NotNull Predicate<LivingEntity> filter
+        double fastSpeed
 ) implements EntityGoal<Mob> {
     /**
      * The deserializer for the goal.
@@ -45,27 +38,22 @@ public record EntityGoalAvoidEntity(
         @Nullable
         public EntityGoalAvoidEntity deserialize(@NotNull final Config config) {
             if (!(
-                    config.has("avoidClass")
+                    config.has("entity")
                             && config.has("fleeDistance")
                             && config.has("slowSpeed")
                             && config.has("fastSpeed")
-                            && config.has("filter")
             )) {
                 return null;
             }
 
             try {
-                TestableEntity filter = Entities.lookup(config.getString("filter"));
+                TestableEntity entity = Entities.lookup(config.getString("entity"));
 
                 return new EntityGoalAvoidEntity(
-                        (Class<? extends LivingEntity>)
-                                Objects.requireNonNull(
-                                        EntityType.valueOf(config.getString("avoidClass").toUpperCase()).getEntityClass()
-                                ),
+                        entity,
                         config.getDouble("fleeDistance"),
                         config.getDouble("slowSpeed"),
-                        config.getDouble("fastSpeed"),
-                        filter::matches
+                        config.getDouble("fastSpeed")
                 );
             } catch (Exception e) {
                 /*
--- eco-core/core-nms/nms-common/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/ai/entity/AvoidEntityGoalFactory.kt
+++ eco-core/core-nms/nms-common/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/ai/entity/AvoidEntityGoalFactory.kt
@@ -3,7 +3,7 @@ package com.willfp.eco.internal.spigot.proxy.common.ai.entity
 import com.willfp.eco.core.entities.ai.entity.EntityGoalAvoidEntity
 import com.willfp.eco.internal.spigot.proxy.common.ai.EntityGoalFactory
 import com.willfp.eco.internal.spigot.proxy.common.ai.toBukkitEntity
-import com.willfp.eco.internal.spigot.proxy.common.ai.toNMSClass
+import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.PathfinderMob
 import net.minecraft.world.entity.ai.goal.AvoidEntityGoal
 import net.minecraft.world.entity.ai.goal.Goal
@@ -12,10 +12,10 @@ object AvoidEntityGoalFactory : EntityGoalFactory<EntityGoalAvoidEntity> {
     override fun create(apiGoal: EntityGoalAvoidEntity, entity: PathfinderMob): Goal {
         return AvoidEntityGoal(
             entity,
-            apiGoal.avoidClass.toNMSClass(),
+            LivingEntity::class.java,
             apiGoal.fleeDistance.toFloat(),
             apiGoal.slowSpeed,
             apiGoal.fastSpeed
-        ) { apiGoal.filter.test(it.toBukkitEntity()) }
+        ) { apiGoal.entity.test(it.toBukkitEntity()) }
     }
 }
