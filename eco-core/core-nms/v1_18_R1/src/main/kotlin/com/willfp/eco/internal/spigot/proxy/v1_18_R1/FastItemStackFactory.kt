7a4891d4e395ccd9a4b78e0ab030fec53e86866b
Auxilor <william.favierparsons1@gmail.com>
2022-03-02 12:48:13 +0000

    Fixed refactoring bugs

--- eco-core/core-nms/v1_18_R1/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/v1_18_R1/FastItemStackFactory.kt
+++ eco-core/core-nms/v1_18_R1/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/v1_18_R1/FastItemStackFactory.kt
@@ -1,12 +1,12 @@
 package com.willfp.eco.internal.spigot.proxy.v1_18_R1
 
 import com.willfp.eco.core.fast.FastItemStack
-import com.willfp.eco.internal.spigot.proxy.v1_18_R1.fast.NMSFastItemStack
 import com.willfp.eco.internal.spigot.proxy.FastItemStackFactoryProxy
+import com.willfp.eco.internal.spigot.proxy.common.fast.EcoFastItemStack
 import org.bukkit.inventory.ItemStack
 
 class FastItemStackFactory : FastItemStackFactoryProxy {
     override fun create(itemStack: ItemStack): FastItemStack {
-        return NMSFastItemStack(itemStack)
+        return EcoFastItemStack(itemStack)
     }
 }
--- eco-core/core-nms/v1_18_R2/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/v1_18_R2/FastItemStackFactory.kt
+++ eco-core/core-nms/v1_18_R2/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/v1_18_R2/FastItemStackFactory.kt
@@ -2,11 +2,11 @@ package com.willfp.eco.internal.spigot.proxy.v1_18_R2
 
 import com.willfp.eco.core.fast.FastItemStack
 import com.willfp.eco.internal.spigot.proxy.FastItemStackFactoryProxy
-import com.willfp.eco.internal.spigot.proxy.v1_18_R2.fast.NMSFastItemStack
+import com.willfp.eco.internal.spigot.proxy.common.fast.EcoFastItemStack
 import org.bukkit.inventory.ItemStack
 
 class FastItemStackFactory : FastItemStackFactoryProxy {
     override fun create(itemStack: ItemStack): FastItemStack {
-        return NMSFastItemStack(itemStack)
+        return EcoFastItemStack(itemStack)
     }
 }
