8f758fb1006dce155013ed846ebbf93cc57f8e82
Auxilor <william.favierparsons1@gmail.com>
2022-03-24 12:21:15 +0000

    Changed bukkit -> eco config mapping

--- eco-core/core-backend/src/main/kotlin/com/willfp/eco/internal/config/EcoConfigFactory.kt
+++ eco-core/core-backend/src/main/kotlin/com/willfp/eco/internal/config/EcoConfigFactory.kt
@@ -8,8 +8,14 @@ import com.willfp.eco.core.config.wrapper.ConfigFactory
 import org.bukkit.configuration.ConfigurationSection
 
 object EcoConfigFactory : ConfigFactory {
-    override fun createConfig(config: ConfigurationSection): Config =
-        createConfig(config.getValues(true), ConfigType.YAML)
+    override fun createConfig(bukkit: ConfigurationSection): Config {
+        val config = createConfig(emptyMap(), ConfigType.YAML)
+        for (key in bukkit.getKeys(true)) {
+            config.set(key, bukkit.get(key))
+        }
+
+        return config
+    }
 
     override fun createConfig(values: Map<String, Any>, type: ConfigType): Config =
         EcoConfigSection(type, values)
