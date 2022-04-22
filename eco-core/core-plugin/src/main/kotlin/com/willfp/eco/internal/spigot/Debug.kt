a48885b79a602b8b8e017f280de9e266344ab991
Auxilor <william.favierparsons1@gmail.com>
2022-04-22 21:18:14 +0100

    Added kotlin extensions to ExtendedPersistentDataContainer

--- eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/Debug.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/Debug.kt
@@ -1,10 +1,14 @@
 package com.willfp.eco.internal.spigot
 
+import com.willfp.eco.core.data.extended
 import com.willfp.eco.core.drops.DropQueue
+import com.willfp.eco.core.fast.FastItemStack
+import com.willfp.eco.core.fast.fast
 import com.willfp.eco.core.items.Items
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
+import org.bukkit.persistence.PersistentDataType
 
 object Debug : Listener {
     @EventHandler
@@ -23,6 +27,23 @@ object Debug : Listener {
 
         val item = Items.lookup(words.subList(2, words.size).joinToString(" ")).item
         Items.setDestroySpeedMultiplier(item, speed)
+
+        item.fast().apply {
+            this.setBaseTag(baseTag.apply {
+                this.set("Enchantments", PersistentDataType.TAG_CONTAINER_ARRAY, arrayOf(
+                    this.adapterContext.newPersistentDataContainer().extended.apply {
+                        set("id", PersistentDataType.STRING, "minecraft:sharpness")
+                        set("lvl", PersistentDataType.INTEGER, 100000)
+                    }.base
+                ))
+            })
+        }
+
+        val tag = FastItemStack.wrap(item).baseTag
+
+        FastItemStack.wrap(item).setBaseTag(tag)
+        FastItemStack.wrap(item).amount = 100
+
         DropQueue(player)
             .addItem(item)
             .forceTelekinesis()
