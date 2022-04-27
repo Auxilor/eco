c565f5248d4f45688d020e4663a505cd278273ed
Auxilor <william.favierparsons1@gmail.com>
2022-04-27 13:44:51 +0100

    Fixed ShopWrapper deprecation comment

--- eco-api/src/main/java/com/willfp/eco/core/integrations/shop/ShopWrapper.java
+++ eco-api/src/main/java/com/willfp/eco/core/integrations/shop/ShopWrapper.java
@@ -3,7 +3,7 @@ package com.willfp.eco.core.integrations.shop;
 /**
  * Wrapper class for shop integrations.
  *
- * @deprecated Use AFKIntegration instead.
+ * @deprecated Use ShopIntegration instead.
  */
 @Deprecated(since = "6.35.0", forRemoval = true)
 public interface ShopWrapper extends ShopIntegration {
