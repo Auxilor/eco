106c9b37fc71faf6f1e8de508538fce8a3e5f115
Auxilor <william.favierparsons1@gmail.com>
2022-04-27 13:11:25 +0100

    Removed unneeded test

--- eco-api/src/test/java/StringUtilsTest.java
+++ eco-api/src/test/java/StringUtilsTest.java
@@ -26,47 +26,4 @@ public class StringUtilsTest {
                 StringUtils.splitAround("one|| two", "||")
         );
     }
-
-    @Test
-    public void testProgressBar() {
-        Assertions.assertEquals(
-                "&a--------",
-                StringUtils.createProgressBar(
-                        '-',
-                        8,
-                        1,
-                        "&a", "&e", "&c"
-                )
-        );
-
-        Assertions.assertEquals(
-                "&a-------&e-",
-                StringUtils.createProgressBar(
-                        '-',
-                        8,
-                        0.99,
-                        "&a", "&e", "&c"
-                )
-        );
-
-        Assertions.assertEquals(
-                "&e-&c-------",
-                StringUtils.createProgressBar(
-                        '-',
-                        8,
-                        0.0001,
-                        "&a", "&e", "&c"
-                )
-        );
-
-        Assertions.assertEquals(
-                "&a----&e-&c---",
-                StringUtils.createProgressBar(
-                        '-',
-                        8,
-                        0.5,
-                        "&a", "&e", "&c"
-                )
-        );
-    }
 }
