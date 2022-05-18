cf152215d31237417ebf0f8bd71975387661b314
Auxilor <william.favierparsons1@gmail.com>
2022-05-18 15:00:23 +0100

    Cleaned up internal

--- eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/eventlisteners/EntityDeathByEntityBuilder.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/eventlisteners/EntityDeathByEntityBuilder.kt
@@ -1,7 +1,6 @@
 package com.willfp.eco.internal.spigot.eventlisteners
 
 import com.willfp.eco.core.events.EntityDeathByEntityEvent
-import org.apache.commons.lang.Validate
 import org.bukkit.Bukkit
 import org.bukkit.entity.Entity
 import org.bukkit.entity.LivingEntity
@@ -10,20 +9,12 @@ import org.bukkit.inventory.ItemStack
 
 internal class EntityDeathByEntityBuilder {
     var victim: LivingEntity? = null
-
     var damager: Entity? = null
-
     var deathEvent: EntityDeathEvent? = null
-
     var drops: List<ItemStack>? = null
-
     var xp = 0
-    fun push() {
 
-        Validate.notNull(victim)
-        Validate.notNull(damager)
-        Validate.notNull(drops)
-        Validate.notNull(deathEvent)
+    fun push() {
         val event = EntityDeathByEntityEvent(victim!!, damager!!, drops!!, xp, deathEvent!!)
         Bukkit.getPluginManager().callEvent(event)
     }
--- eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/eventlisteners/EntityDeathByEntityListeners.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/eventlisteners/EntityDeathByEntityListeners.kt
@@ -7,7 +7,6 @@ import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityDeathEvent
-import java.util.concurrent.atomic.AtomicReference
 
 class EntityDeathByEntityListeners(
     private val plugin: EcoPlugin
@@ -35,25 +34,24 @@ class EntityDeathByEntityListeners(
         }
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     fun onEntityDeath(event: EntityDeathEvent) {
         val victim = event.entity
         val drops = event.drops
         val xp = event.droppedExp
 
-        val atomicBuiltEvent = AtomicReference<EntityDeathByEntityBuilder>(null)
+        var builtEvent: EntityDeathByEntityBuilder? = null
 
         for (builder in events) {
             if (builder.victim == victim) {
-                atomicBuiltEvent.set(builder)
+                builtEvent = builder
             }
         }
 
-        if (atomicBuiltEvent.get() == null) {
+        if (builtEvent == null) {
             return
         }
 
-        val builtEvent = atomicBuiltEvent.get()
         events.remove(builtEvent)
         builtEvent.drops = drops
         builtEvent.xp = xp
