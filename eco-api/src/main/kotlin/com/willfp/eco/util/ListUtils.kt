b835988eecdd01a98922c2431f417244f190057d
Auxilor <william.favierparsons1@gmail.com>
2022-05-28 14:32:35 +0100

    Added toSingletonList

--- eco-api/src/main/kotlin/com/willfp/eco/util/ListUtils.kt
+++ eco-api/src/main/kotlin/com/willfp/eco/util/ListUtils.kt
@@ -2,20 +2,18 @@
 
 package com.willfp.eco.util
 
-/**
- * @see ListUtils.listToFrequencyMap
- */
+/** @see ListUtils.listToFrequencyMap */
 fun <T> List<T>.toFrequencyMap(): Map<T, Int> =
     ListUtils.listToFrequencyMap(this)
 
-/**
- * @see ListUtils.containsIgnoreCase
- */
+/** @see ListUtils.containsIgnoreCase */
 fun Iterable<String>.containsIgnoreCase(element: String): Boolean =
     ListUtils.containsIgnoreCase(this, element)
 
-/**
- * @see ListUtils.create2DList
- */
+/** @see ListUtils.create2DList */
 fun <T> create2DList(rows: Int, columns: Int): MutableList<MutableList<T>> =
     ListUtils.create2DList(rows, columns)
+
+/** @see ListUtils.toSingletonList */
+fun <T> T.toSingletonList(): List<T> =
+    ListUtils.toSingletonList(this)
