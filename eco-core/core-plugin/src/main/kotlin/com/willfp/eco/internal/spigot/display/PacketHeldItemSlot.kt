9a703f61904d20f90c790121a476843e8c9479ea
Auxilor <william.favierparsons1@gmail.com>
2022-09-26 14:02:20 +0100

    One more held item slot fix

--- eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/display/PacketHeldItemSlot.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/display/PacketHeldItemSlot.kt
@@ -5,7 +5,6 @@ import com.comphenix.protocol.events.PacketContainer
 import com.comphenix.protocol.events.PacketEvent
 import com.willfp.eco.core.AbstractPacketAdapter
 import com.willfp.eco.core.EcoPlugin
-import com.willfp.eco.core.display.Display
 import com.willfp.eco.internal.spigot.display.frame.DisplayFrame
 import com.willfp.eco.internal.spigot.display.frame.lastDisplayFrame
 import org.bukkit.entity.Player
@@ -13,6 +12,14 @@ import org.bukkit.entity.Player
 class PacketHeldItemSlot(plugin: EcoPlugin) :
     AbstractPacketAdapter(plugin, PacketType.Play.Server.HELD_ITEM_SLOT, false) {
     override fun onSend(
+        packet: PacketContainer,
+        player: Player,
+        event: PacketEvent
+    ) {
+        player.lastDisplayFrame = DisplayFrame.EMPTY
+    }
+
+    override fun onReceive(
         packet: PacketContainer,
         player: Player,
         event: PacketEvent
