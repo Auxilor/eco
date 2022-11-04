d0baf50709abdb0781517d48b800e6f6f7de8590
Auxilor <william.favierparsons1@gmail.com>
2022-11-04 15:07:34 +0000

    Fixed illusioner goals

--- eco-core/core-nms/nms-common/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/ai/entity/IllusionerBlindnessSpellGoalFactory.kt
+++ eco-core/core-nms/nms-common/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/ai/entity/IllusionerBlindnessSpellGoalFactory.kt
@@ -1,14 +1,14 @@
 package com.willfp.eco.internal.spigot.proxy.common.ai.entity
 
-import com.willfp.eco.core.entities.ai.entity.EntityGoalLeapAtTarget
+import com.willfp.eco.core.entities.ai.entity.EntityGoalIllusionerBlindnessSpell
 import com.willfp.eco.internal.spigot.proxy.common.ai.EntityGoalFactory
 import com.willfp.eco.internal.spigot.proxy.common.ai.opengoals.IllusionerBlindnessSpellGoal
 import net.minecraft.world.entity.PathfinderMob
 import net.minecraft.world.entity.ai.goal.Goal
 import net.minecraft.world.entity.monster.Illusioner
 
-object IllusionerBlindnessSpellGoalFactory : EntityGoalFactory<EntityGoalLeapAtTarget> {
-    override fun create(apiGoal: EntityGoalLeapAtTarget, entity: PathfinderMob): Goal? {
+object IllusionerBlindnessSpellGoalFactory : EntityGoalFactory<EntityGoalIllusionerBlindnessSpell> {
+    override fun create(apiGoal: EntityGoalIllusionerBlindnessSpell, entity: PathfinderMob): Goal? {
         if (entity !is Illusioner) return null
 
         return IllusionerBlindnessSpellGoal(
--- eco-core/core-nms/nms-common/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/ai/entity/IllusionerMirrorSpellGoalFactory.kt
+++ eco-core/core-nms/nms-common/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/ai/entity/IllusionerMirrorSpellGoalFactory.kt
@@ -1,14 +1,14 @@
 package com.willfp.eco.internal.spigot.proxy.common.ai.entity
 
-import com.willfp.eco.core.entities.ai.entity.EntityGoalLeapAtTarget
+import com.willfp.eco.core.entities.ai.entity.EntityGoalIllusionerMirrorSpell
 import com.willfp.eco.internal.spigot.proxy.common.ai.EntityGoalFactory
 import com.willfp.eco.internal.spigot.proxy.common.ai.opengoals.IllusionerMirrorSpellGoal
 import net.minecraft.world.entity.PathfinderMob
 import net.minecraft.world.entity.ai.goal.Goal
 import net.minecraft.world.entity.monster.Illusioner
 
-object IllusionerMirrorSpellGoalFactory : EntityGoalFactory<EntityGoalLeapAtTarget> {
-    override fun create(apiGoal: EntityGoalLeapAtTarget, entity: PathfinderMob): Goal? {
+object IllusionerMirrorSpellGoalFactory : EntityGoalFactory<EntityGoalIllusionerMirrorSpell> {
+    override fun create(apiGoal: EntityGoalIllusionerMirrorSpell, entity: PathfinderMob): Goal? {
         if (entity !is Illusioner) return null
 
         return IllusionerMirrorSpellGoal(
