b5cd8f42e063cea235bd2971412c3b48e2f3afeb
Auxilor <william.favierparsons1@gmail.com>
2022-11-05 21:25:38 +0000

    Fixed illusioner goals

--- eco-core/core-nms/nms-common/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/ai/entity/IllusionerBlindnessSpellGoalFactory.kt
+++ eco-core/core-nms/nms-common/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/ai/entity/IllusionerBlindnessSpellGoalFactory.kt
@@ -2,7 +2,6 @@ package com.willfp.eco.internal.spigot.proxy.common.ai.entity
 
 import com.willfp.eco.core.entities.ai.entity.EntityGoalIllusionerBlindnessSpell
 import com.willfp.eco.internal.spigot.proxy.common.ai.EntityGoalFactory
-import com.willfp.eco.internal.spigot.proxy.common.ai.opengoals.IllusionerBlindnessSpellGoal
 import net.minecraft.world.entity.PathfinderMob
 import net.minecraft.world.entity.ai.goal.Goal
 import net.minecraft.world.entity.monster.Illusioner
@@ -11,11 +10,14 @@ object IllusionerBlindnessSpellGoalFactory : EntityGoalFactory<EntityGoalIllusio
     override fun create(apiGoal: EntityGoalIllusionerBlindnessSpell, entity: PathfinderMob): Goal? {
         if (entity !is Illusioner) return null
 
-        return IllusionerBlindnessSpellGoal(
-            entity
-        )
+        // Have to use reflection for it to work
+        return Illusioner::class.java.declaredClasses[1]
+            .getDeclaredConstructor(Illusioner::class.java)
+            .apply { isAccessible = true }
+            .newInstance(entity) as Goal
     }
 
-    override fun isGoalOfType(goal: Goal) = goal is IllusionerBlindnessSpellGoal
-            || goal::class.java.name.contains("IllusionerBlindnessSpellGoal")
+    override fun isGoalOfType(goal: Goal): Boolean {
+        return Illusioner::class.java.declaredClasses[1].isInstance(goal)
+    }
 }
--- eco-core/core-nms/nms-common/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/ai/entity/IllusionerMirrorSpellGoalFactory.kt
+++ eco-core/core-nms/nms-common/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/ai/entity/IllusionerMirrorSpellGoalFactory.kt
@@ -2,7 +2,6 @@ package com.willfp.eco.internal.spigot.proxy.common.ai.entity
 
 import com.willfp.eco.core.entities.ai.entity.EntityGoalIllusionerMirrorSpell
 import com.willfp.eco.internal.spigot.proxy.common.ai.EntityGoalFactory
-import com.willfp.eco.internal.spigot.proxy.common.ai.opengoals.IllusionerMirrorSpellGoal
 import net.minecraft.world.entity.PathfinderMob
 import net.minecraft.world.entity.ai.goal.Goal
 import net.minecraft.world.entity.monster.Illusioner
@@ -11,11 +10,14 @@ object IllusionerMirrorSpellGoalFactory : EntityGoalFactory<EntityGoalIllusioner
     override fun create(apiGoal: EntityGoalIllusionerMirrorSpell, entity: PathfinderMob): Goal? {
         if (entity !is Illusioner) return null
 
-        return IllusionerMirrorSpellGoal(
-            entity
-        )
+        // Have to use reflection for it to work
+        return Illusioner::class.java.declaredClasses[0]
+            .getDeclaredConstructor(Illusioner::class.java)
+            .apply { isAccessible = true }
+            .newInstance(entity) as Goal
     }
 
-    override fun isGoalOfType(goal: Goal) = goal is IllusionerMirrorSpellGoal
-            || goal::class.java.name.contains("IllusionerMirrorSpellGoal")
+    override fun isGoalOfType(goal: Goal): Boolean {
+        return Illusioner::class.java.declaredClasses[0].isInstance(goal)
+    }
 }
