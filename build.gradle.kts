bd7594a1178caa0d66a284a296fb6938f2b53c2e
Auxilor <william.favierparsons1@gmail.com>
2022-12-12 12:13:22 +0000

    Fixed 1.19.3 build

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation(project(path = ":eco-core:core-nms:v1_18_R1", configuration = "reobf"))
     implementation(project(path = ":eco-core:core-nms:v1_18_R2", configuration = "reobf"))
     implementation(project(path = ":eco-core:core-nms:v1_19_R1", configuration = "reobf"))
+    implementation(project(path = ":eco-core:core-nms:v1_19_R2", configuration = "reobf"))
 }
 
 allprojects {
