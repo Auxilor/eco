bc24aaeb3c4d6caabf3bfbb103b1c4c998b26e4d
Auxilor <william.favierparsons1@gmail.com>
2023-03-15 21:06:02 +0000

    Fixed 1.19.4 support
    (cherry picked from commit ad47b7898e1d6628f53bd0ee68c91c1acf74e1c0)

--- eco-api/src/main/java/com/willfp/eco/core/proxy/ProxyConstants.java
+++ eco-api/src/main/java/com/willfp/eco/core/proxy/ProxyConstants.java
@@ -22,7 +22,8 @@ public final class ProxyConstants {
             "v1_18_R1",
             "v1_18_R2",
             "v1_19_R1",
-            "v1_19_R2"
+            "v1_19_R2",
+            "v1_19_R3"
     );
 
     private ProxyConstants() {
