40aec26f15fae85442d9272d6884c8fea6df9362
FireML <106587317+FireML@users.noreply.github.com>
2023-04-27 23:03:34 +0100

    Initial Denizen Support

---
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/integrations/customitems/CustomItemsDenizen.kt
@@ -0,0 +1,35 @@
+package com.willfp.eco.internal.spigot.integrations.customitems
+
+import com.willfp.eco.core.integrations.customitems.CustomItemsIntegration
+import com.willfp.eco.core.items.CustomItem
+import com.willfp.eco.core.items.TestableItem
+import com.willfp.eco.core.items.provider.ItemProvider
+import com.willfp.eco.util.NamespacedKeyUtils
+import com.denizenscript.denizen.objects.ItemTag;
+import com.willfp.eco.core.items.Items
+import org.bukkit.event.Listener
+
+class CustomItemsDenizen(
+) : CustomItemsIntegration, Listener {
+
+    override fun registerProvider() {
+        Items.registerItemProvider(DenizenProvider())
+    }
+
+    override fun getPluginName(): String {
+        return "Denizen"
+    }
+
+    private class DenizenProvider : ItemProvider("denizen") {
+        override fun provideForKey(key: String): TestableItem? {
+            val item = ItemTag.valueOf(key, false) ?: return null
+            val id = item.scriptName
+            val namespacedKey = NamespacedKeyUtils.create("denizen", id)
+            return CustomItem(
+                namespacedKey,
+                { id.equals(ItemTag(it).scriptName, ignoreCase = true) },
+                item.itemStack
+            )
+        }
+    }
+}
