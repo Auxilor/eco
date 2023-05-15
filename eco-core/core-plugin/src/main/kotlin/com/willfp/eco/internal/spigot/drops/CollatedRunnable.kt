d35df75125abeabcf6ca499e941b52dab099941c
mani123 <70480001+mani1232@users.noreply.github.com>
2023-05-15 15:23:08 +0200

    Fix

--- eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/drops/CollatedRunnable.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/drops/CollatedRunnable.kt
@@ -7,21 +7,25 @@ import com.willfp.eco.internal.drops.EcoFastCollatedDropQueue
 class CollatedRunnable(plugin: EcoPlugin) {
     init {
         plugin.scheduler.runTimer({
-            for ((key, value) in EcoFastCollatedDropQueue.COLLATED_MAP) {
-                val queue = EcoDropQueue(key)
-                    .setLocation(value.location)
-                    .addItems(value.drops)
-                    .addXP(value.xp)
+            if (EcoFastCollatedDropQueue.COLLATED_MAP.isNotEmpty()) {
+                for ((key, value) in EcoFastCollatedDropQueue.COLLATED_MAP) {
+                    val queue = EcoDropQueue(key)
+                        .setLocation(value.location)
+                        .addItems(value.drops)
+                        .addXP(value.xp)
 
-                if (value.telekinetic) {
-                    queue.forceTelekinesis()
+                    plugin.scheduler.run(value.location) {
+                        if (value.telekinetic) {
+                            queue.forceTelekinesis()
+                        }
+
+                        queue.push()
+                    }
+
+                    EcoFastCollatedDropQueue.COLLATED_MAP.remove(key)
                 }
-
-                queue.push()
-
-                EcoFastCollatedDropQueue.COLLATED_MAP.remove(key)
+                EcoFastCollatedDropQueue.COLLATED_MAP.clear()
             }
-            EcoFastCollatedDropQueue.COLLATED_MAP.clear()
         }, 1, 1)
     }
 }
