48ef87cb4da8d9879b1e247971505f560ec367bd
mani123 <70480001+mani1232@users.noreply.github.com>
2023-06-12 17:48:08 +0200

    snakeyml update

--- eco-core/core-backend/build.gradle.kts
+++ eco-core/core-backend/build.gradle.kts
@@ -5,11 +5,11 @@ dependencies {
     // Libraries
     implementation("org.reflections:reflections:0.9.12")
     implementation("org.objenesis:objenesis:3.2")
+    compileOnly("org.yaml:snakeyaml:2.0")
 
     compileOnly("org.spigotmc:spigot-api:1.17.1-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.10.10")
     compileOnly("net.kyori:adventure-text-minimessage:4.10.0")
     compileOnly("net.kyori:adventure-platform-bukkit:4.1.0")
-    compileOnly("org.yaml:snakeyaml:1.33")
     compileOnly("com.moandjiezana.toml:toml4j:0.7.2")
 }
