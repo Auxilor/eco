008030799dd259d31e61bf10ec2bcc2367b09f29
Will FP <william.favierparsons1@gmail.com>
2025-08-01 09:55:44 +0100

    Bumped protocollib version

--- eco-api/build.gradle.kts
+++ eco-api/build.gradle.kts
@@ -5,7 +5,7 @@ dependencies {
     // Other
     compileOnly("org.spigotmc:spigot-api:1.20.2-R0.1-SNAPSHOT")
     compileOnly("commons-lang:commons-lang:2.6")
-    compileOnly("com.comphenix.protocol:ProtocolLib:4.6.1-SNAPSHOT")
+    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
     compileOnly("com.google.code.gson:gson:2.8.8")
 }
 
