e255a39fa464c1c5367824a90b332b8d17768788
Will FP <william.favierparsons1@gmail.com>
2025-08-01 09:59:38 +0100

    Fixed bad PR

--- eco-core/core-nms/modern/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/modern/NewEcoFastItemStack.kt
+++ eco-core/core-nms/modern/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/common/modern/NewEcoFastItemStack.kt
@@ -76,8 +76,9 @@ open class NewEcoFastItemStack(
         enchantment: Enchantment,
         checkStored: Boolean
     ): Int {
-        val minecraft = CraftRegistry.bukkitToMinecraftHolder<Enchantment, Registries.ENCHANTMENT>(
-            enchantment
+        val minecraft = CraftRegistry.bukkitToMinecraftHolder(
+            enchantment,
+            Registries.ENCHANTMENT
         )
 
         val enchantments = handle.get(DataComponents.ENCHANTMENTS) ?: return 0
