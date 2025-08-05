1def83e29ffd4c58c50b12fba3a71bef1e22f4bd
DeltaEon <59774164+DeltaEon@users.noreply.github.com>
2025-08-04 20:35:07 -0700

    Update NewerEcoFastItemStack.kt
    Forgot an import. fixed.

--- eco-core/core-nms/v1_21_7/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/v1_21_7/item/NewerEcoFastItemStack.kt
+++ eco-core/core-nms/v1_21_7/src/main/kotlin/com/willfp/eco/internal/spigot/proxy/v1_21_7/item/NewerEcoFastItemStack.kt
@@ -3,6 +3,7 @@ package com.willfp.eco.internal.spigot.proxy.v1_21_7.item
 import com.willfp.eco.internal.spigot.proxy.common.modern.NewEcoFastItemStack
 import net.minecraft.core.component.DataComponents
 import net.minecraft.world.item.component.TooltipDisplay
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 
