b0f9b16e8186e084f1bc61da4bf6c3dbc9621b48
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-03-06 08:35:15 +0000

    Refactor playIfEnabled to use Config interface instead of EcoPlugin

--- eco-api/src/main/kotlin/com/willfp/eco/util/SoundConfigUtils.kt
+++ eco-api/src/main/kotlin/com/willfp/eco/util/SoundConfigUtils.kt
@@ -1,16 +1,15 @@
 package com.willfp.eco.util
 
-import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.config.interfaces.Config
 import org.bukkit.SoundCategory
 import org.bukkit.entity.Player
 
 object SoundConfigUtils {
-    fun playIfEnabled(plugin: EcoPlugin, player: Player, configPath: String) {
-        val sound = SoundUtils.getSound(plugin.configYml.getFormattedString("$configPath.sound")) ?: return
-        val pitch = plugin.configYml.getDoubleFromExpression("$configPath.pitch")
-        val volume = plugin.configYml.getDoubleFromExpression("$configPath.volume")
-        var categoryString = plugin.configYml.getFormattedStringOrNull("$configPath.category")
+    fun playIfEnabled(config: Config, player: Player, configPath: String) {
+        val sound = SoundUtils.getSound(config.getFormattedString("$configPath.sound")) ?: return
+        val pitch = config.getDoubleFromExpression("$configPath.pitch")
+        val volume = config.getDoubleFromExpression("$configPath.volume")
+        var categoryString = config.getFormattedStringOrNull("$configPath.category")
         if (categoryString == null) categoryString = "MASTER"
         val category = SoundCategory.valueOf(categoryString.uppercase())
 
