4b3140b105b21390568e43862721701094ab09eb
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-04-14 23:15:08 +0100

    fix(M20): Use coordinate-based keys in PlayerBlockListener

--- eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/data/PlayerBlockListener.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/data/PlayerBlockListener.kt
@@ -101,7 +101,7 @@ class PlayerBlockListener(
     }
 
     private fun writeKey(location: Location) {
-        val loc = location.hashCode().toString(16)
+        val loc = "${location.blockX}_${location.blockY}_${location.blockZ}"
         location.chunk.persistentDataContainer.set(
             plugin.createNamespacedKey(loc.lowercase()),
             PersistentDataType.INTEGER,
@@ -114,7 +114,7 @@ class PlayerBlockListener(
     }
 
     private fun removeKey(location: Location) {
-        val loc = location.hashCode().toString(16)
+        val loc = "${location.blockX}_${location.blockY}_${location.blockZ}"
         location.chunk.persistentDataContainer.remove(plugin.createNamespacedKey(loc.lowercase()))
     }
 }
