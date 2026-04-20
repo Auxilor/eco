dc08d61fa6b1f04ce870d0d75120afcbb4984688
NicoNeko <nicuch100@gmail.com>
2026-04-20 19:41:55 +0300

    Change vein allowed blocks to TestableBlock

--- eco-api/src/main/java/com/willfp/eco/util/BlockUtils.java
+++ eco-api/src/main/java/com/willfp/eco/util/BlockUtils.java
@@ -1,5 +1,6 @@
 package com.willfp.eco.util;
 
+import com.willfp.eco.core.blocks.TestableBlock;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -12,6 +13,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * Utilities / API methods for blocks.
@@ -20,19 +22,19 @@ public final class BlockUtils {
     /**
      * Get a set of all blocks in contact with each other of a specific type.
      *
-     * @param start            The initial block.
-     * @param allowedMaterials A list of all valid {@link Material}s.
-     * @param limit            The maximum size of vein to return.
+     * @param start         The initial block.
+     * @param allowedBlocks A list of all valid {@link TestableBlock}s.
+     * @param limit         The maximum size of vein to return.
      * @return A set of all {@link Block}s.
      */
     @NotNull
     public static Set<Block> getVein(@NotNull final Block start,
-                                     @NotNull final List<Material> allowedMaterials,
+                                     @NotNull final List<TestableBlock> allowedBlocks,
                                      final int limit) {
         Set<Block> blocks = new HashSet<>();
         Queue<Block> toProcess = new LinkedList<>();
 
-        if (allowedMaterials.contains(start.getType())) {
+        if (allowedBlocks.stream().anyMatch(testableBlock -> testableBlock.matches(start))) {
             toProcess.add(start);
         }
 
@@ -48,7 +50,8 @@ public final class BlockUtils {
             for (BlockFace face : BlockFace.values()) {
                 Block adjacentBlock = currentBlock.getRelative(face);
 
-                if (!blocks.contains(adjacentBlock) && allowedMaterials.contains(adjacentBlock.getType())) {
+                if (!blocks.contains(adjacentBlock) &&
+                        allowedBlocks.stream().anyMatch(testableBlock -> testableBlock.matches(adjacentBlock))) {
                     toProcess.add(adjacentBlock);
                 }
             }
