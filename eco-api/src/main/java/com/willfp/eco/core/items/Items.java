a7e82763244baa93f55501a4396279781e5985f4
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-05-02 15:21:51 +0100

    feat: Update getVein method to use TestableBlock instead of Material (#499)
    * feat: Update getVein method to use TestableBlock instead of Material
    
    * fix: Correct tag extraction logic in Items.java
    
    * fix: Remove incorrect import of Preconditions in NaturalExpGainBuilder.kt

--- eco-api/src/main/java/com/willfp/eco/core/items/Items.java
+++ eco-api/src/main/java/com/willfp/eco/core/items/Items.java
@@ -214,7 +214,7 @@ public final class Items {
         boolean isTag = base.startsWith("#");
 
         if (isTag) {
-            String tag = args[0].substring(1);
+            String tag = split[0].substring(1);
             ItemTag itemTag = TAGS.get(tag);
 
             if (itemTag == null) {
