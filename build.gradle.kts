3e7491af2a1bb8d203988b6030bc224e13b6d18b
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2026-05-02 15:42:03 +0100

    fix: Removed CombatLogX from internal repositories.

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,13 +43,9 @@ allprojects {
 
         maven("https://repo.auxilor.io/repository/maven-public/")
 
-        // CombatLogX (must be before JitPack to exclude sirblobman from JitPack)
-        maven("https://nexus.sirblobman.xyz/public/")
-
         maven("https://jitpack.io") {
             content {
                 includeGroupByRegex("com\\.github\\..*")
-                excludeGroup("com.github.sirblobman")
                 excludeGroup("com.github.TownyAdvanced")
             }
         }
