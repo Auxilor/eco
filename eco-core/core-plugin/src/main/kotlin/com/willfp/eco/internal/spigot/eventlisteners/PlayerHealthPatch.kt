fcb21f0ae3997e6a6825165a550a5c37338c89b7
0ft3n <dkoften@gmail.com>
2026-05-27 00:13:55 +0300

    Fixed health patch to respect current health attribute value (#525)

--- eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/eventlisteners/PlayerHealthPatch.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/eventlisteners/PlayerHealthPatch.kt
@@ -3,11 +3,13 @@ package com.willfp.eco.internal.spigot.eventlisteners
 import com.willfp.eco.core.Eco
 import com.willfp.eco.util.saveHealth
 import com.willfp.eco.util.savedHealth
+import org.bukkit.attribute.Attribute
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import kotlin.math.min
 
 object PlayerHealthPatch: Listener {
     @EventHandler(priority = EventPriority.LOWEST)
@@ -20,7 +22,9 @@ object PlayerHealthPatch: Listener {
         if (Eco.get().ecoPlugin.configYml.getBool("enable-health-fix")) {
             Eco.get().ecoPlugin.scheduler.runLater(5L) {
                 if (!event.player.isDead) {
-                    event.player.health = event.player.savedHealth
+                    event.player.health = min(event.player.savedHealth,
+                        event.player.getAttribute(Attribute.MAX_HEALTH)?.value
+                            ?: 20.0)
                 }
             }
         }
