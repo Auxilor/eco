68cb5d7382f5674acc97835c3b5f2235a5dda0c6
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-07-30 15:40:23 +0100

    fix: credit kills finished by indirect damage

--- eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/eventlisteners/EntityDeathByEntityListeners.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/eventlisteners/EntityDeathByEntityListeners.kt
@@ -1,6 +1,7 @@
 package com.willfp.eco.internal.spigot.eventlisteners
 
 import com.willfp.eco.core.EcoPlugin
+import org.bukkit.Bukkit
 import org.bukkit.entity.LivingEntity
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
@@ -11,52 +12,37 @@ import org.bukkit.event.entity.EntityDeathEvent
 class EntityDeathByEntityListeners(
     private val plugin: EcoPlugin
 ) : Listener {
-    private val events = mutableSetOf<EntityDeathByEntityBuilder>()
+    private val windowTicks = plugin.configYml.getInt("kill-attribution.window-ticks").toLong()
+
+    private val tracker = LastDamagerTracker(windowTicks) { Bukkit.getCurrentTick().toLong() }
+
+    init {
+        plugin.scheduler.runTimer(windowTicks.coerceAtLeast(1), windowTicks.coerceAtLeast(1)) {
+            tracker.purgeExpired()
+        }
+    }
 
     @EventHandler(priority = EventPriority.HIGH)
     fun onEntityDamage(event: EntityDamageByEntityEvent) {
-        if ((event.entity !is LivingEntity)) {
+        if (event.entity !is LivingEntity) {
             return
         }
 
-        val victim = event.entity as LivingEntity
-
-        if (victim.health > event.finalDamage) {
-            return
-        }
-
-        val builtEvent = EntityDeathByEntityBuilder()
-        builtEvent.victim = victim
-        builtEvent.damager = event.damager
-
-        events += builtEvent
-
-        this.plugin.scheduler.runLater(5) { // Fixes conflicts with WildStacker
-            events.remove(builtEvent)
-        }
+        tracker.record(event.entity.uniqueId, event.damager)
     }
 
     @EventHandler(priority = EventPriority.HIGH)
     fun onEntityDeath(event: EntityDeathEvent) {
         val victim = event.entity
-        val drops = event.drops
-        val xp = event.droppedExp
+        val damager = tracker.resolve(victim.uniqueId) ?: return
 
-        var builtEvent: EntityDeathByEntityBuilder? = null
+        tracker.forget(victim.uniqueId)
 
-        for (builder in events) {
-            if (builder.victim == victim) {
-                builtEvent = builder
-            }
-        }
-
-        if (builtEvent == null) {
-            return
-        }
-
-        events.remove(builtEvent)
-        builtEvent.drops = drops
-        builtEvent.xp = xp
+        val builtEvent = EntityDeathByEntityBuilder()
+        builtEvent.victim = victim
+        builtEvent.damager = damager
+        builtEvent.drops = event.drops
+        builtEvent.xp = event.droppedExp
         builtEvent.deathEvent = event
 
         builtEvent.push()
