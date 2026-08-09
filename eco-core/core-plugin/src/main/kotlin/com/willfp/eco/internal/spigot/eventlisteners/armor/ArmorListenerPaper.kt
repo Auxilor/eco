a4c8ea1331501cbe0ef1f1b2ee829ddec3ce67f3
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2026-08-09 11:59:26 +0100

    chore: Inline import

--- eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/eventlisteners/armor/ArmorListenerPaper.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/eventlisteners/armor/ArmorListenerPaper.kt
@@ -1,5 +1,6 @@
 package com.willfp.eco.internal.spigot.eventlisteners.armor
 
+import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent
 import com.willfp.eco.core.events.ArmorEquipEvent
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
@@ -12,7 +13,7 @@ import org.bukkit.event.Listener
  */
 class ArmorListenerPaper : Listener {
     @EventHandler(priority = EventPriority.MONITOR)
-    fun onArmorChange(event: com.destroystokyo.paper.event.player.PlayerArmorChangeEvent) {
+    fun onArmorChange(event: PlayerArmorChangeEvent) {
         Bukkit.getPluginManager().callEvent(ArmorEquipEvent(event.player))
     }
 }
