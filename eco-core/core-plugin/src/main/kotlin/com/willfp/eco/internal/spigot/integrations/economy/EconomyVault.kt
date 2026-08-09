f85f3cb55a4b019638d8783ee8be21042ae57adb
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2026-08-09 12:10:08 +0100

    fix: Dynamic Economy

--- eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/integrations/economy/EconomyVault.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/eco/internal/spigot/integrations/economy/EconomyVault.kt
@@ -3,25 +3,27 @@ package com.willfp.eco.internal.spigot.integrations.economy
 import com.willfp.eco.core.integrations.economy.EconomyIntegration
 import java.math.BigDecimal
 import net.milkbowl.vault.economy.Economy
+import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 
-class EconomyVault(
-    private val vault: Economy
-) : EconomyIntegration {
+class EconomyVault : EconomyIntegration {
+    private val vault: Economy?
+        get() = Bukkit.getServer().servicesManager.getRegistration(Economy::class.java)?.provider
+
     override fun hasAmount(player: OfflinePlayer, amount: BigDecimal): Boolean {
-        return vault.has(player, amount.toDouble())
+        return vault?.has(player, amount.toDouble()) ?: false
     }
 
     override fun giveMoney(player: OfflinePlayer, amount: BigDecimal): Boolean {
-        return vault.depositPlayer(player, amount.toDouble()).transactionSuccess()
+        return vault?.depositPlayer(player, amount.toDouble())?.transactionSuccess() ?: false
     }
 
     override fun removeMoney(player: OfflinePlayer, amount: BigDecimal): Boolean {
-        return vault.withdrawPlayer(player, amount.toDouble()).transactionSuccess()
+        return vault?.withdrawPlayer(player, amount.toDouble())?.transactionSuccess() ?: false
     }
 
     override fun getExactBalance(player: OfflinePlayer): BigDecimal {
-        return vault.getBalance(player).toBigDecimal()
+        return vault?.getBalance(player)?.toBigDecimal() ?: BigDecimal.ZERO
     }
 
     override fun getPluginName(): String {
