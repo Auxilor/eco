4b7249e2f6a81eb95a07a82cca246ee2da1b4f23
Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>
2026-08-31 18:35:13 +0100

    feat: Remove ProtocolLib

--- eco-api/src/main/java/com/willfp/eco/core/Prerequisite.java
+++ eco-api/src/main/java/com/willfp/eco/core/Prerequisite.java
@@ -41,17 +41,6 @@ public class Prerequisite {
     );
 
     /**
-     * Requires the server to have ProtocolLib installed.
-     *
-     * @deprecated ProtocolLib is no longer used by eco, AbstractPacketAdapter has been marked for removal since 6.77.0.
-     */
-    @Deprecated(since = "6.77.0", forRemoval = true)
-    public static final Prerequisite HAS_PROTOCOLLIB = new Prerequisite(
-            () -> ClassUtils.exists("com.comphenix.protocol.events.PacketAdapter"),
-            "Requires server to have ProtocolLib"
-    );
-
-    /**
      * Requires the server to be running at least 1.21.3.
      *
      * @deprecated eco requires 1.21.4+, this is always true.
